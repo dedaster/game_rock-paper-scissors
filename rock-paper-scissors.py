0da5f05cb30b314b2697176adb83dfcc4c09b988
dedaster <ded_pank@bk.ru>
2022-12-07 07:06:35 +0300

    add external correction

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -35,45 +35,42 @@ def paper():
 def comp_choice(player_ch):
     comp_ch = random.choice(['Камень','Ножницы','Бумага'])
     comp = tk.Label(window, text = comp_ch, width = w, height = h, bg = "white",).grid(row = 3, column = 2)
-    l = ["Камень", "Ножницы", "Бумага"]
+    opt = ["Камень", "Ножницы", "Бумага"]
     win = ["Победа за тобой!", "Поражение", "Ничья"]
-    if player_ch == l[0] and comp_ch == l[1]:
+    if player_ch == opt[0] and comp_ch == opt[1]:
         winner = win[0]
-    elif player_ch == l[0] and comp_ch == l[2]:
+    elif player_ch == opt[0] and comp_ch == opt[2]:
         winner = win[1]
-    elif player_ch == l[1] and comp_ch == l[0]:
+    elif player_ch == opt[1] and comp_ch == opt[0]:
         winner = win[1]
-    elif player_ch == l[1] and comp_ch == l[2]:
+    elif player_ch == opt[1] and comp_ch == opt[2]:
         winner = win[0]
-    elif player_ch == l[2] and comp_ch == l[0]:
+    elif player_ch == opt[2] and comp_ch == opt[0]:
         winner = win[0]
-    elif player_ch == l[2] and comp_ch == l[1]:
+    elif player_ch == opt[2] and comp_ch == opt[1]:
         winner = win[1]
     else:
         winner = win[2]
         
     # вывод результата
     if winner == win[0]:
-        res = tk.Label(window, text = winner, width = w, height = h, bg = "white", fg = "green").grid(row = 0, column = 1)
+        res = tk.Label(window, text = winner, width = w*3, height = h, bg = "white", fg = "green").grid(row = 0, columnspan = 3)
     elif winner == win[1]:
-        res = tk.Label(window, text = winner, width = w, height = h, bg = "white", fg = "red").grid(row = 0, column = 1)
+        res = tk.Label(window, text = winner, width = w*3, height = h, bg = "white", fg = "red").grid(row = 0, columnspan = 3)
     else:
-        res = tk.Label(window, text = winner, width = w, height = h, bg = "white", fg = "black").grid(row = 0, column = 1)
+        res = tk.Label(window, text = winner, width = w*3, height = h, bg = "white", fg = "black").grid(row = 0, columnspan = 3)
     
     # списки комментариев к результатам
     reswin = random.choice(["Так держать!", "Ты потрясающий!", "Невероятный манёвр!"])
     reslose = random.choice(["Повезёт в другой раз!", "Всё ещё впереди!", "У тебя получится!"])
     resdraw = random.choice(["Вот это схватка!", "Встретились 2 титана!", "Какое напряжение!"])
     
-    
     if winner == win[0]:
-        comments = tk.Label(window, text = reswin, width = w, height = 1).grid(row = 1, column = 1)
+        comments = tk.Label(window, text = reswin, width = w, height = 1).grid(row = 1, column = 1, pady = 5)
     elif winner == win[1]:
-        comments = tk.Label(window, text = reslose, width = w, height = 1).grid(row = 1, column = 1)
+        comments = tk.Label(window, text = reslose, width = w, height = 1).grid(row = 1, column = 1, pady = 5)
     else:
-        comments = tk.Label(window, text = resdraw, width = w, height = 1).grid(row = 1, column = 1)
-
-        
+        comments = tk.Label(window, text = resdraw, width = w, height = 1).grid(row = 1, column = 1, pady = 5)
 #внешний вид окна
 window = tk.Tk()
 window.title("Камень-ножницы-бумага. GOTY Edition")
@@ -82,16 +79,16 @@ tk.Label(window, text = "Твой ход:", width = w, height = 1, fg = "black")
 tk.Label(window, width = w, height = 1, fg = "black").grid(row = 2, column = 1)
 tk.Label(window, text = "Ход компьютера:", width = w, height = 1, fg = "black").grid(row = 2, column = 2)
 # меню результатов игры
-res = tk.Label(window, text = "Кто победит?", width = w, height = h, bg = "white", fg = "black").grid(row = 0, column = 1)
-comments = tk.Label(window, text = "Что же ждёт нас сегодня?", width = w, height = 1).grid(row = 1, column = 1)
+res = tk.Label(window, text = "Кто победит?", width = w*3, height = h, bg = "white", fg = "black").grid(row = 0, columnspan = 3)
+comments = tk.Label(window, text = "Что же ждёт нас сегодня?", width = w, height = 1).grid(row = 1, column = 1, pady = 5)
 player = tk.Label(window, width = w, height = h, bg = "white", fg = "black").grid(row = 3, column = 0)
 vs = tk.Label(window, text = "против", width = w, height = h, bg = "white", fg = "black").grid(row = 3, column = 1)
 comp = tk.Label(window, width = w, height = h, bg = "white", fg = "black").grid(row = 3, column = 2)
 #кнопки выбора
-btn_rock = tk.Button(text = "Камень", width = w, height = h, bg = "white", fg = "black", command = rock).grid(row = 4, column = 0)
+btn_rock = tk.Button(text = "Камень", width = w, height = h, bg = "white", fg = "black", command = rock).grid(row = 4, column = 0, pady = 5)
 btn_scissors = tk.Button(text = "Ножницы", width = w, height = h, bg = "white", fg = "black", command = scissors).grid(row = 4, column = 1)
 btn_paper = tk.Button(text = "Бумага", width = w, height = h, bg = "white", fg = "black", command = paper).grid(row = 4, column = 2)
 # кнопка выхода из игры
-button = tk.Button(text = "Выйти из игры", width = w, height = 1, bg = "white", fg = "black", command = close_game).grid(row = 5, column = 2)
+button = tk.Button(text = "Выйти из игры", width = w, height = 1, bg = "white", fg = "black", command = close_game).grid(row = 5, column = 2, pady = 5)
 
 window.mainloop()
